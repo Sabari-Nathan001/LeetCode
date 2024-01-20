39ad6a779a91ceb98daa0ce38b21dd939fe231b5
Sabari_Nathan_G <107342025+Sabari-Nathan001@users.noreply.github.com>
2024-01-20 17:50:56 +0530

    Time: 3 ms (17.22%), Space: 41.5 MB (30.51%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(st.size()>0)
+            {
+                if((st.peek()=='(' && s.charAt(i)==')')||(st.peek()=='{' && s.charAt(i)=='}')||(st.peek()=='[' && s.charAt(i)==']')){
+                    st.pop();
+                    continue;
+                }
+            }
+            st.push(s.charAt(i));
+        }
+        if(st.isEmpty())
+            return true;
+        return false;
+    }
+}
