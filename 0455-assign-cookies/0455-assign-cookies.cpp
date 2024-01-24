6214081e0f5f4955e56e8b45ab6dfe8d9d8519d7
Sabari_Nathan_G <107342025+Sabari-Nathan001@users.noreply.github.com>
2024-01-24 20:49:49 +0530

    Time: 15 ms (98.96%), Space: 20.4 MB (7.31%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -6,7 +6,7 @@ public:
         int c=0;
         int i=0,j=0;
         while(i<g.size() && j<s.size()){
-            if (s[j]>=g[i]){
+            if(s[j]>=g[i]){
                 c++;
                 i++;
             }
