17e812aba130116f64505bd2bfa3dc02ce780f53
Sabari_Nathan_G <107342025+Sabari-Nathan001@users.noreply.github.com>
2024-02-07 18:50:50 +0530

    Time: 796 ms (31.67%), Space: 192.4 MB (12.28%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        //bruteforce
+        // set<vector<int>> s;
+        // vector<vector<int>> v;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         for(int k=0;k<n;k++){
+        //             if(i!=j && j!=k && k!=i){
+        //                 if(nums[i]+nums[j]+nums[k]==0){
+        //                    vector<int> temp={nums[i],nums[j],nums[k]};
+        //                    sort(temp.begin(),temp.end());
+        //                    s.insert(temp);  
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // for(auto &it : s){
+        //     v.push_back(it);
+        // }
+        // return v;
+        //optimal
         int target=0;
         sort(nums.begin(),nums.end());
         set<vector<int>> s;
@@ -10,20 +32,22 @@ public:
             int right=nums.size()-1;
             while(left<right){
                 int sum=nums[i]+nums[left]+nums[right];
-                if(sum==target){
+                if(sum==0){
                     s.insert({nums[i],nums[left],nums[right]});
                     left++;
                     right--;
                 }
                 else if(sum<target){
                     left++;
-                } else {
+                }
+                else{
                     right--;
                 }
             }
         }
-        for(auto it:s)
+        for(auto &it : s){
             v.push_back(it);
+        }
         return v;
     }
 };
