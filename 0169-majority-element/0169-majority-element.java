ac9e7d733a4b81b322c18884a6249fe55a9adacb
Sabari_Nathan_G <107342025+Sabari-Nathan001@users.noreply.github.com>
2024-02-09 19:26:30 +0530

    Time: 4 ms (55.33%), Space: 50.8 MB (14.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,10 @@
 class Solution {
     public int majorityElement(int[] nums) {
-	    // if (nums.length == 1) {
-	    // return nums[0];
-	    // }
-	    // Arrays.sort(nums);
-	    // return nums[nums.length / 2];
+	    if (nums.length == 1) {
+	    return nums[0];
+	    }
+	    Arrays.sort(nums);
+	    return nums[nums.length / 2];
         //Moore Voting Algo
-        int c=0,ans=0;
-        for(int num : nums){
-            if(c==0)
-                ans=num;
-            if(num==ans)
-                c+=1;
-            else
-                c-=1;
-        }
-        return ans;
     }
 }
