890db4c84112c7f6ee63e4f156126de09dbc20cc
Sabari_Nathan_G <107342025+Sabari-Nathan001@users.noreply.github.com>
2024-02-09 15:04:17 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (36.84%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
-        if (m == 0) // Check if the matrix is empty
-            return false;
+//         if (m == 0) // Check if the matrix is empty
+//             return false;
         
         int n = matrix[0].length;
         int left = 0;
