23e750aecd01927c07fef4706097f9b47fbddc8e
Sabari_Nathan_G <107342025+Sabari-Nathan001@users.noreply.github.com>
2024-03-16 07:48:47 +0530

    Time: 14 ms (64.18%), Space: 48.7 MB (39.09%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,53 +1,15 @@
 class Solution {
-    public class Pair{
-        int val;
-        int count;
-
-        Pair(int val, int count) {
-            this.val = val;
-            this.count = count;
-        }
-    }
-
     public int[] topKFrequent(int[] nums, int k) {
-        List<Integer> res = new ArrayList<>();
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> {
-            return a.count - b.count;
-        });
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1);
-            } else {
-                map.put(nums[i], 1);
-            }
+        Map<Integer,Integer> mp=new HashMap<>();
+        for(int num : nums){
+            mp.put(num,mp.getOrDefault(num,0)+1);
         }
-
-        for (int i : map.keySet()) {
-            Pair p = new Pair(i, map.get(i));
-            if (pq.size() < k) {
-                pq.add(p);
-            } else {
-                if (p.count > pq.peek().count) {
-                    pq.remove();
-                    pq.add(p);
-                }
-            }
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->mp.get(b)-mp.get(a));
+        pq.addAll(mp.keySet());
+        int[] arr=new int[k];
+        for(int i=0;i<k;i++) {
+            arr[i]=pq.poll();
         }
-
-        while (!pq.isEmpty()) {
-            Pair p = pq.poll();
-            res.add(p.val);
-        }
-
-        int[] ans = new int[res.size()];
-
-        for (int i = 0; i < ans.length; i++) {
-            ans[i] = res.get(i);
-        }
-
-        return ans;
+        return arr;
     }
 }
