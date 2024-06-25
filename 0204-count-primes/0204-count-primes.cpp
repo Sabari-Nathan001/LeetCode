65fd9ce76512702de0c96a2c0748ea1589cb5afe
Sabari_Nathan_G <107342025+Sabari-Nathan001@users.noreply.github.com>
2024-06-25 09:13:02 +0530

    Time: 672 ms (5.09%), Space: 11.5 MB (44.65%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -5,7 +5,7 @@ public:
             return 0;
         vector<bool> ans(n+1,true);
         ans[0]=false;ans[1]=false;
-        for(int i=2;i*i<n;i++){
+        for(int i=2;i<n;i++){
             if(ans[i]==true){
                 for(int j=2*i;j<n;j+=i){
                 ans[j]=false;
