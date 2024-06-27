5d6e85b236fa39964b88438b88a18f45a48c3576
Sabari_Nathan_G <107342025+Sabari-Nathan001@users.noreply.github.com>
2024-06-27 09:47:20 +0530

    Time: 1 ms (100.00%), Space: 42.1 MB (50.44%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        char carry='0';
+        StringBuilder sb = new StringBuilder();
+        char a1[] = a.toCharArray();
+        char b1[] = b.toCharArray();
+        int i=a1.length-1,j=b1.length-1;
+        while(i>=0 || j>=0){
+            char tempA = i>=0 ? a1[i] : '0';
+            char tempB = j>=0 ? b1[j] : '0';
+            if(tempA==tempB){
+                if(carry=='0'){
+                    sb.append("0");
+                }
+                else{
+                    sb.append("1");
+                }
+                carry=tempA;
+            }
+            else if(tempA!=tempB){
+                if(carry=='0'){
+                    sb.append("1");
+                }
+                else{
+                    sb.append("0");
+                    carry='1';
+                }
+            }
+            i--;j--;
+        }
+        if(carry=='1'){
+            sb.append("1");
+        }
+        sb.reverse();
+        return sb.toString();
+    }
+}
+ 
